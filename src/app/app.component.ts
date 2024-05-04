fe6af52720aea5a8c1d2377ef4238167a5086483
Israelamat <israelamatgil@gmail.com>
2024-05-04 05:14:15 +0200

    home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MainMenuComponent } from './main-menu/main-menu.component';
+import { HeroComponent } from './hero/hero.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [RouterOutlet, CommonModule, RouterOutlet, MainMenuComponent, HeroComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
